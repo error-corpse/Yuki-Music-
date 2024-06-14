e2591b27b4e235670cb644a523f069ab12074506
error-corpse <123547302+error-corpse@users.noreply.github.com>
2024-06-14 10:32:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Add Owner Username without @ 
 OWNER_USERNAME = getenv("OWNER_USERNAME", "corpsealone")
 # Get Your bot username
-BOT_USERNAME = getenv("BOT_USERNAME", "yukii_music_bot")
+BOT_USERNAME = getenv("BOT_USERNAME", "makima_mubot")
 # Don't Add style font 
 BOT_NAME = getenv("BOT_NAME", "YuKi MUSIC")
 #get Your Assistant User name
